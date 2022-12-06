4d896541c2d4ffb8cbf148b2c45aa0a7d1d711bf
Tri-Luan <luancao511@email.com>
2022-12-06 17:24:52 +0700

    Update DishViewForm

--- ProjectHotpot/DishViewForm.cs
+++ ProjectHotpot/DishViewForm.cs
@@ -1,10 +1,12 @@
-﻿using ProjectHotpot.BUS;
+﻿using FontAwesome.Sharp;
+using ProjectHotpot.BUS;
 using ProjectHotpot.DTO;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +29,7 @@ namespace ProjectHotpot
         {
             tvCategory.ShowLines = false;
             LoadTreeView();
+
             //Button addButton = new Button();
             //addButton.Text = "Add";
             //addButton.Location = new Point(70, 70);
@@ -51,7 +54,7 @@ namespace ProjectHotpot
                     //imageList1.ImageSize = new Size(68,68);
                     item.ImageKey = dish.DishID.ToString();
                     item.SubItems.Add(dish.DishName);
-                    item.SubItems.Add(dish.DishPrice.ToString());
+                    item.SubItems.Add(dish.DishPrice.ToString("C0", CultureInfo.GetCultureInfo("vi-vn")));
                     listViewDish.Items.Add(item);
                 }
             }
@@ -59,9 +62,40 @@ namespace ProjectHotpot
             {
                 MessageBox.Show("Cannot Load Data!!!");
             }
+            //Set currency VND
+            dgvDishes.Columns[2].DefaultCellStyle.Format = "C0";
+            dgvDishes.Columns[2].DefaultCellStyle.FormatProvider = CultureInfo.GetCultureInfo("vi-vn");
+            dgvDishes.Columns[3].DefaultCellStyle.Format = "C0";
+            dgvDishes.Columns[3].DefaultCellStyle.FormatProvider = CultureInfo.GetCultureInfo("vi-vn");
         }
 
-        public void LoadTreeView()
+        public void AddItem(Dish dish)
+        {
+            foreach (DataGridViewRow item in dgvDishes.Rows)
+            {
+                if (item.Cells[0].Value.ToString() == dish.DishName)
+                {
+                    item.Cells[1].Value = int.Parse(item.Cells[1].Value.ToString()) + 1;
+                    item.Cells[3].Value = (int.Parse(item.Cells[1].Value.ToString()) * double.Parse(item.Cells[2].Value.ToString()));
+                    CalculateTotal();
+                    return;
+                }
+            }
+            dgvDishes.Rows.Add(new object[] { dish.DishName, 1, dish.DishPrice, dish.DishPrice, "Delete" });
+            CalculateTotal();
+        }
+
+    public void CalculateTotal()
+    {
+        double total = 0;
+        foreach (DataGridViewRow item in dgvDishes.Rows)
+            {
+                total += double.Parse(item.Cells[3].Value.ToString());
+            }
+            txtTotal.Text = total.ToString("C0", CultureInfo.GetCultureInfo("vi-vn"));
+    }
+
+    public void LoadTreeView()
         {
             List<DishCategory> cates = new DishCategoryBUS().GetAll();
             foreach(var cate in cates)
@@ -77,7 +111,12 @@ namespace ProjectHotpot
             if (listViewDish.SelectedItems.Count > 0)
             {
                 ListViewItem item = listViewDish.SelectedItems[0];
-                MessageBox.Show("Thêm món "+item.Text+" thành công");
+
+                Dish dish = new Dish();
+                dish.DishName = item.SubItems[1].Text;
+                //Convert currency to int
+                dish.DishPrice = int.Parse(item.SubItems[2].Text, NumberStyles.Currency, new CultureInfo("vi-vn"));
+                AddItem(dish);
             }
         }
 
@@ -115,6 +154,60 @@ namespace ProjectHotpot
                 list.Images.Add(HelperMethod.ConvertBinaryToImage(dish.Image));
             }
             return list;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnPay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void dgvDishes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 4) //Delete
+            {
+                DialogResult dialogResult = MessageBox.Show("Are you sure?", "Confirmation", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                   
+                }
+            }
+        }
+
+        private void dgvDishes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgvDishes_CellMouseMove(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //if (e.ColumnIndex == 4)
+            //{
+            //    dgvDishes.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Brown;
+            //}
+        }
+
+        private void dgvDishes_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            //if (e.ColumnIndex == 4)
+            //{
+            //    dgvDishes.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Brown;
+            //}
         }
     }
 }
